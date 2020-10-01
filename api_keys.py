7ec02d83577cf9a4c5a73c021f75bac3208fbad6
sapiesitupou <66816965+sapiesitupou@users.noreply.github.com>
2020-10-01 09:38:18 -0700

    Update api_keys.py

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "ff6b87dd6684b608ef0f3ed816cbd13a"
+weather_api_key = "YOUR KEY HERE!"
 
 # Google API Key
-g_key = "AIzaSyDqmr8M1sDYR60Nc32MLgANHtlLAJF3RdM"
+g_key = "YOUR KEY HERE!"
